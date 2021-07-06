1efe918588c9dadeb1e8132c908e79fb60f014c4
Mehmet <guel_mehmet@hotmail.de>
2021-07-06 23:47:41 +0200

    Updated visualization to be cellsize independent and support float values

--- src/visualization.py
+++ src/visualization.py
@@ -8,12 +8,13 @@ from IPython.display import display
 
 
 class Visualizer:
-    def __init__(self, simulation: Simulation, cell_width: int, cell_height: int):
+    def __init__(self, simulation: Simulation, cell_width: int, cell_height: int,canvas_width: int,canvas_height:int):
         self.simulation = simulation
         self.cell_width = cell_width
         self.cell_height = cell_height
-        self.canvas = Canvas(width=self.simulation.topography.width *
-                             cell_width, height=self.simulation.topography.height * cell_height)
+        #self.canvas = Canvas(width=self.simulation.topography.width *
+        #                    cell_width, height=self.simulation.topography.height * cell_height)
+        self.canvas = Canvas(width = canvas_width,height = canvas_height)
 
     def draw(self, step: int):
         '''
@@ -21,10 +22,11 @@ class Visualizer:
         :param step: Which simulation step of the simulation should be drawn
         :return: canvas with the drawn image
         '''
+        unit_width = self.canvas.width/self.simulation.topography.width
+        unit_height = self.canvas.height/self.simulation.topography.height
         self.canvas.clear()
         with hold_canvas(self.canvas):
-            indices = np.indices(
-                (self.simulation.topography.width, self.simulation.topography.height))
+            indices = np.indices((int(self.canvas.width/self.cell_width),int(self.canvas.height/self.cell_height)))
             row_indices = indices[0].flatten()
             column_indices = indices[1].flatten()
             self.canvas.stroke_rects(
@@ -33,39 +35,40 @@ class Visualizer:
                 width=self.cell_width,
                 height=self.cell_height
             )
-
+            '''
             pedestrians_coordinates = [
                 s for s in self.simulation.simulation_steps[step].values()]
             self.canvas.fill_style = 'red'
             self.canvas.fill_rects(
-                x=[p.x * self.cell_width for p in pedestrians_coordinates],
-                y=[p.y * self.cell_height for p in pedestrians_coordinates],
+                x=[round(p.x*unit_width) for p in pedestrians_coordinates],
+                y=[round(p.y*unit_height) for p in pedestrians_coordinates],
                 width=self.cell_width - 1,
                 height=self.cell_height - 1
             )
+            '''
             # draw obstacles
             self.canvas.fill_style = 'black'
             self.canvas.fill_rects(
-                x=[p.x * self.cell_width for p in self.simulation.topography.obstacles],
-                y=[p.y * self.cell_height for p in self.simulation.topography.obstacles],
-                width=[p.width * self.cell_width for p in self.simulation.topography.obstacles],
-                height=[p.height * self.cell_height for p in self.simulation.topography.obstacles]
+                x=[round(p.x*unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.obstacles],
+                y=[round(p.y*unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.obstacles],
+                width=[round(p.width * unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.obstacles],
+                height=[round(p.height * unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.obstacles]
             )
             # draw target
             self.canvas.fill_style = 'green'
             self.canvas.fill_rects(
-                x=[p.x * self.cell_width for p in self.simulation.topography.targets],
-                y=[p.y * self.cell_height for p in self.simulation.topography.targets],
-                width=[p.width * self.cell_width for p in self.simulation.topography.targets],
-                height=[p.height * self.cell_height for p in self.simulation.topography.targets]
+                x=[round(p.x*unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.targets],
+                y=[round(p.y*unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.targets],
+                width=[round(p.width * unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.targets],
+                height=[round(p.height * unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.targets]
             )
             # draw sources
             self.canvas.fill_style = 'blue'
             self.canvas.fill_rects(
-                x=[p.x * self.cell_width for p in self.simulation.topography.sources],
-                y=[p.y * self.cell_height for p in self.simulation.topography.sources],
-                width=[p.width * self.cell_width for p in self.simulation.topography.sources],
-                height=[p.height * self.cell_height for p in self.simulation.topography.sources]
+                x=[round(p.x*unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.sources],
+                y=[round(p.y*unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.sources],
+                width=[round(p.width* unit_width/self.cell_width)*self.cell_width for p in self.simulation.topography.sources],
+                height=[round(p.height*unit_height/self.cell_height)*self.cell_height for p in self.simulation.topography.sources]
             )
         display(self.canvas)
 
